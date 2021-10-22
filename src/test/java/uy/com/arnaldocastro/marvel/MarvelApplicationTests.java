69118515d7992d4856f12d53e746e68dd1034efd
AGO-90 <andres.ghione@gmail.com>
2021-10-22 00:47:16 -0300

    Initial commit

---
+++ src/test/java/uy/com/arnaldocastro/marvel/MarvelApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.com.arnaldocastro.marvel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
